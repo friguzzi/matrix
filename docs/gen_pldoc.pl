3ae0a853a7f34e06ec9907419c5fed39234c6878
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2023-12-11 08:47:45 +0100

    new pldoc gen

--- docs/gen_pldoc.pl
+++ docs/gen_pldoc.pl
@@ -1,5 +1,23 @@
-:- use_module('../prolog/matrix.pl').
+:- use_module(library(pldoc)).
+:- use_module(library(doc_files)).
+:- use_module(library(filesex)).
 
-:- doc_save('../prolog/matrix.pl',[doc_root('.'), index_file(index)]).
+:- initialization(gen_doc, main).
 
-:- halt.
+doc_file(matrix).
+
+load_all :-
+    ensure_loaded(library(clpr)),
+    forall(doc_file(File),
+           ( directory_file_path('../prolog', File, Path),
+             ensure_loaded(Path))).
+
+gen_doc :-
+    load_all,
+    Opts = [ doc_root('.'),
+             include_reexported(false)
+           ],
+
+    forall(doc_file(File),
+           ( directory_file_path('../prolog', File, Path),
+             doc_save(Path, Opts))).
